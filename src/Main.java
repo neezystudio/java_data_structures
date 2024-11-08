ac33ceca05c6fb7b17838d1d09730612002eeb1f
neeystudio <victorneez6@gmail.com>
2024-11-08 13:40:51 +0300

    insertion,deletion and finding an element at a constant time

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] groceries={"Apples","Avocado","watermelon","beetroot","Kale", "Sukumawezi"};
+         System.out.println("Number of groceries" +groceries.length);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+         for(String item:groceries){
+             System.out.println(item + ",");
+         }
+         System.out.println();
+         //retrieve the element in constant time
+        System.out.println("grocery item at index 3:" + groceries[3]);
+        //remove element
+        groceries[3] = null;
+        for(String item:groceries){
+            System.out.println(item + ",");
         }
     }
 }
